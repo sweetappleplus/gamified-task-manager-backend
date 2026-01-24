c230b24a4bb2566371bb208f62303cfd2a663461
sweetappleplus <sweetappleplus@gmail.com>
2026-01-24 09:31:45 +0200

    feat(user): implement UserService with CRUD operations

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller.js';
 import { AppService } from './app.service.js';
 import { PrismaService } from './prisma/prisma.service.js';
+import { UserService } from './user/user.service.js';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService, PrismaService],
+  providers: [AppService, PrismaService, UserService],
 })
 export class AppModule {}
