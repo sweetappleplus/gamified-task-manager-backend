696204321007e2090377913535d651105e95aa7a
sweetappleplus <sweetappleplus@gmail.com>
2026-01-25 19:51:09 +0200

    refactor(app): remove user module and related files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,9 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller.js';
 import { AppService } from './app.service.js';
 import { PrismaService } from '../prisma/prisma.service.js';
-import { UserModule } from '../user/user.module.js';
 
 @Module({
-  imports: [UserModule],
+  imports: [],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
