b43daca1cad324f6dee0d82d57648755dde8bae7
sweetappleplus <sweetappleplus@gmail.com>
2026-01-25 17:35:23 +0200

    chore(consts): reorganize environment variable management

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module.js';
-import { PORT } from '../consts/index.js';
+import { PORT } from './consts/index.js';
 
 if (!PORT) {
   throw new Error("PORT is not set in the environment variables");
