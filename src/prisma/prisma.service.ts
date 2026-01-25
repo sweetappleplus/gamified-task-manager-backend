de72f930a6d97162860c4c50d9b1a4de0ec62bfd
sweetappleplus <sweetappleplus@gmail.com>
2026-01-25 18:00:21 +0200

    feat(prisma): integrate DATABASE_URL for database connection

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,12 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from '../generated/prisma/client.js';
+import { DATABASE_URL } from '../consts/index.js';
+
+if (!DATABASE_URL) {
+  throw new Error("DATABASE_URL is not set in the environment variables");
+}
 
 @Injectable()
 export class PrismaService extends PrismaClient {
   constructor() {
     const adapter = new PrismaPg({
-      connectionString: process.env.DATABASE_URL as string,
+      connectionString: DATABASE_URL,
     });
     super({ adapter });
   }
