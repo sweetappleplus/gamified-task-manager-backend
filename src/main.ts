4f4c8168df6515f94ec7f7e5b5e6caced9e8c30b
sweetappleplus <sweetappleplus@gmail.com>
2026-01-25 02:32:57 +0200

    chore(config): add dotenv for environment variable management

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
+import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module.js';
+import { PORT } from '../consts/index.js';
+
+if (!PORT) {
+  throw new Error("PORT is not set in the environment variables");
+}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3001);
+  await app.listen(Number(PORT));
 }
 bootstrap();
