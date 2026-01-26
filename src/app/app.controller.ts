0c3e7e1f0059ccbcc18a63703c62ac22e998f4fb
sweetappleplus <sweetappleplus@gmail.com>
2026-01-26 16:52:51 +0200

    feat(api): implement health check endpoint

--- src/app/app.controller.ts
+++ src/app/app.controller.ts
@@ -1,12 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service.js';
+import type { ApiResponse } from '../types/index.js';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('health')
+  getHealth(): ApiResponse<void> {
+    return this.appService.getHealth();
   }
 }
--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { API_STATUSES } from '../consts/index.js';
+import { ApiResponse } from '../types/index.js';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getHealth(): ApiResponse<void> {
+    return {
+      status: API_STATUSES.SUCCESS,
+      message: 'The application is running',
+      timestamp: new Date().toISOString(),
+    };
   }
 }
