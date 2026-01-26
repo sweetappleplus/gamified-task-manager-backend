a4d68df26d9831f2cac44dff61f64d81a521d40e
sweetappleplus <sweetappleplus@gmail.com>
2026-01-26 19:12:27 +0200

    feat(consts): add OTP and SMTP configuration constants

--- src/consts/index.ts
+++ src/consts/index.ts
@@ -1,3 +1,4 @@
 export * from './api.js';
 export * from './db.js';
 export * from './log.js';
+export * from './otp.js';
---
+++ src/consts/otp.ts
@@ -0,0 +1,13 @@
+export const OTP_EXPIRATION_TIME_MINUTES = process.env.OTP_EXPIRATION_TIME_MINUTES;
+
+export const MAX_OTP_ATTEMPTS = process.env.MAX_OTP_ATTEMPTS;
+
+export const SMTP_FROM = process.env.SMTP_FROM;
+
+export const SMTP_HOST = process.env.SMTP_HOST;
+
+export const SMTP_PORT = process.env.SMTP_PORT;
+
+export const SMTP_USER = process.env.SMTP_USER;
+
+export const SMTP_PASS = process.env.SMTP_PASS;
