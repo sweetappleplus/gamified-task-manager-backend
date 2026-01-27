c68c4d1a1d4cd880423175442b62f03b8bfbc0e0
sweetappleplus <sweetappleplus@gmail.com>
2026-01-27 19:23:35 +0200

    refactor(prisma): remove isActive column from BonusConfig model

---
+++ prisma/migrations/20260127172327_remove_is_active_bonus_config/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `isActive` on the `BonusConfig` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "BonusConfig" DROP COLUMN "isActive";
