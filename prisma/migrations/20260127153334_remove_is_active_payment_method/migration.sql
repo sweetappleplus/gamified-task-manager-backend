c96fc29b346db3b6ea52cdef16376c47b2f3696b
sweetappleplus <sweetappleplus@gmail.com>
2026-01-27 17:33:47 +0200

    refactor(payment-method): drop isActive column from PaymentMethod table

---
+++ prisma/migrations/20260127153334_remove_is_active_payment_method/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `isActive` on the `PaymentMethod` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "PaymentMethod" DROP COLUMN "isActive";
