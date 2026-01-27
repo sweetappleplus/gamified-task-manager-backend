b58235901eafcb183c0c26bc02a6664cb8fa0559
sweetappleplus <sweetappleplus@gmail.com>
2026-01-27 11:58:24 +0200

    feat(system-setting): add Prisma service to system setting module

--- src/system-setting/system-setting.module.ts
+++ src/system-setting/system-setting.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { SystemSettingService } from './system-setting.service.js';
 import { SystemSettingController } from './system-setting.controller.js';
+import { PrismaService } from '../prisma/prisma.service.js';
 
 @Module({
   controllers: [SystemSettingController],
-  providers: [SystemSettingService],
+  providers: [SystemSettingService, PrismaService],
 })
 export class SystemSettingModule {}
